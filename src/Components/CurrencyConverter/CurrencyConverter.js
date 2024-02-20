94b1fe3af9f466f39876c4ff84d54a7740950a15
Antony Perez <varadme@yahoo.com>
2024-02-19 21:32:48 -0800

    fixed console error

--- src/Components/CurrencyConverter/CurrencyConverter.js
+++ src/Components/CurrencyConverter/CurrencyConverter.js
@@ -8,18 +8,22 @@ function LineChart({ baseCurrency, selectedCurrency }) {
   const chartInstance = useRef(null)
 
   useEffect(() => {
-    fetch(
-      `https://api.frankfurter.app/2020-01-01..2022-01-01?from=${baseCurrency}&to=${selectedCurrency}`,
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        const chartLabels = Object.keys(data.rates)
-        const chartData = Object.values(data.rates).map(
-          (rate) => rate[selectedCurrency],
-        )
-        setChartData({ labels: chartLabels, data: chartData })
-      })
-      .catch((error) => console.error(error))
+    if (baseCurrency !== selectedCurrency) {
+      fetch(
+        `https://api.frankfurter.app/2020-01-01..2022-01-01?from=${baseCurrency}&to=${selectedCurrency}`,
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.rates) {
+            const chartLabels = Object.keys(data.rates)
+            const chartData = Object.values(data.rates).map(
+              (rate) => rate[selectedCurrency],
+            )
+            setChartData({ labels: chartLabels, data: chartData })
+          }
+        })
+        .catch((error) => console.error(error))
+    }
   }, [baseCurrency, selectedCurrency])
 
   useEffect(() => {
